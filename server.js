79d14a94281711bfbe5a3942c0771292b3b63870
Dvd <44392695+Dvd622@users.noreply.github.com>
2022-05-16 15:38:03 +1000

    update

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ multer = require("multer")
 require('dotenv').config();
 const saltRounds=10
 const controller=require("./serverController")
-const jwt = require("jsonwebtoken")
 
 //app
 const app=express();
@@ -31,12 +30,12 @@ app.get('/user',(req, res)=>{
 })
 
 //find a user with username: return password
-app.post('/login',(req, res)=>{
-    controller.PostLogin(req, res)
+app.get('/login/:username',(req, res)=>{
+    controller.FindUsername(req, res)
 })
 
 //find specific user with user ID or username
-app.get('/user/:id',validateToken, (req, res) => {
+app.get('/user/:id', (req, res) => {
     controller.FindUser(req, res)
 })
 
@@ -50,13 +49,6 @@ app.patch('/user/:id',(req, res)=>{
     controller.UpdateUser(req, res)
 })
 
-app.post("/refreshToken", (req,res) => {
-    controller.RefreshToken(req,res)
-})
-
-app.delete("/logout", (req,res) => {
-    controller.Logout(req,res)
-})
 // Set storage
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -77,23 +69,6 @@ app.patch("/user/:id/uploadphoto",upload.single('myImage'),(req,res)=>{
 app.get('/user/:id/picture', (req,res,next)=> {
     controller.GetUserPicture(req, res)
 });
-
-function validateToken(req, res, next) {
-    //get token from request header
-    const authHeader = req.headers["authorization"]
-    const token = authHeader.split(" ")[1]
-    //the request header contains the token "Bearer <token>", split the string and use the second value in the split array.
-    if (token == null) res.sendStatus(400).send("Token not present")
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) {
-            res.status(403).send("Token invalid")
-        }
-        else {
-            req.username = user
-            next() //proceed to the next action in the calling function
-        }
-    }) //end of jwt.verify()
-}
 
 //listen to 8080 port
 app.listen(process.env.SERVER_PORT,function(req,res){
